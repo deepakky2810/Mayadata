92846f0ffd3f8f407a10fb0a7418d6df915ce796
Deepak Kumar Yadav <deepakky2810@gmail.com>
2021-04-19 06:02:52 +0530

    updated personal details

--- src/components/personalDetails/PersonalDetails.js
+++ src/components/personalDetails/PersonalDetails.js
@@ -16,10 +16,11 @@ const PersonalDetails = () => {
                 <p className="p-1">Name: Deepak Kumar Yadav</p>
                 <p>Roll: 1805483</p>
                 <p>Github Profile: <a href="https://github.com/deepakky2810" target="_blank" rel="noreferrer">Click to visit</a></p>
-                <p>Skills: C</p>
+                <p>Skills: DS, Algorithms, C, C++, Computer Networks, OS(Theory and Practical), Javascript, ReactJs</p>
                 <p className="p-2">Projects:</p>
                 <p>1. Eco-friendly Portable Cooling System</p>
                 <p>2. Invoice Management System</p>
+                <p>3. Console based Snake Game(C++)(from scratch)</p>
 
             </div>
         </div>
